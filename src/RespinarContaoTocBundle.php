2932fcc5ef7dc15a4ea7154bf4c8c0c04d17c007
Hamid Peywasti <hamid@respinar.com>
2023-11-19 12:29:55 +0330

    create bundle

---
+++ src/RespinarContaoTocBundle.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao Table of Contents Bundle.
+ *
+ * (c) Hamid Peywasti 2023 <hamid@respinar.com>
+ *
+ * @license MIT
+ */
+
+namespace Respinar\ContaoTocBundle;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class RespinarContaoTocBundle extends Bundle
+{
+    public function getPath(): string
+    {
+        return \dirname(__DIR__);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function build(ContainerBuilder $container): void
+    {
+        parent::build($container);
+    }
+}
