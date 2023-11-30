68579bd97b53fccc5d1f8bf8e6f7f43e4ca75b92
Hamid Peywasti <hamid@respinar.com>
2023-11-30 17:02:23 +0330

    convert to function with options

--- public/js/toc.js
+++ public/js/toc.js
@@ -2,53 +2,56 @@
  ---Table of Contents Js ------
  --- TechinDetail.com ------
 */
-window.addEventListener("DOMContentLoaded", (event) => {
-  const article = document.getElementsByClassName("layout_full")[0];
-  const headings = article.querySelectorAll("h2, h3");
-  const toc = document.getElementById("toc");
-  const totalHeadings = headings.length;
 
-  let tocOl = document.createElement("ol");
-  let tocFragment = new DocumentFragment();
-  let mainLi = null;
-  let subUl = null;
-  let subLi = null;
-  let isSibling = false;
+function toc(className, headingSelector) {
+  window.addEventListener("DOMContentLoaded", (event) => {
+    const article = document.getElementsByClassName(className)[0];
+    const headings = article.querySelectorAll(headingSelector);
+    const toc = document.getElementById("toc");
+    const totalHeadings = headings.length;
 
-  if (totalHeadings > 1) {
-    for (let element of headings) {
-      let anchor = document.createElement("a");
-      let anchorText = element.innerText;
-      anchor.innerText = anchorText;
-      let elementId = anchorText.replaceAll(" ", "-").toLowerCase();
-      anchor.href = "#" + elementId;
-      element.id = elementId;
-      let level = element.nodeName;
+    let tocOl = document.createElement("ol");
+    let tocFragment = new DocumentFragment();
+    let mainLi = null;
+    let subUl = null;
+    let subLi = null;
+    let isSibling = false;
 
-      if ("H3" === level) {
-        if (mainLi) {
-          subLi = document.createElement("li");
-          subLi.appendChild(anchor);
+    if (totalHeadings > 1) {
+      for (let element of headings) {
+        let anchor = document.createElement("a");
+        let anchorText = element.innerText;
+        anchor.innerText = anchorText;
+        let elementId = anchorText.replaceAll(" ", "-").toLowerCase();
+        anchor.href = window.location.pathname + "#" + elementId;
+        element.id = elementId;
+        let level = element.nodeName;
 
-          if (isSibling === false) {
-            subUl = document.createElement("ul");
+        if ("H3" === level) {
+          if (mainLi) {
+            subLi = document.createElement("li");
+            subLi.appendChild(anchor);
+
+            if (isSibling === false) {
+              subUl = document.createElement("ul");
+            }
+            subUl.appendChild(subLi);
+            mainLi.appendChild(subUl);
+
+            isSibling = true;
           }
-          subUl.appendChild(subLi);
-          mainLi.appendChild(subUl);
-
-          isSibling = true;
+        } else {
+          mainLi = document.createElement("li");
+          mainLi.appendChild(anchor);
+          tocFragment.appendChild(mainLi);
+          isSibling = false;
+          subUl = null;
         }
-      } else {
-        mainLi = document.createElement("li");
-        mainLi.appendChild(anchor);
-        tocFragment.appendChild(mainLi);
-        isSibling = false;
-        subUl = null;
       }
+      tocOl.append(tocFragment);
+      toc.append(tocOl);
+    } else {
+      toc.style.display = "none";
     }
-    tocOl.append(tocFragment);
-    toc.append(tocOl);
-  } else {
-    toc.style.display = "none";
-  }
-});
+  })
+}
