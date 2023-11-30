2edc0ab7a9939df569939828f90a16f4cddccda9
Hamid Peywasti <hamid@respinar.com>
2023-11-30 17:02:46 +0330

    add backend template

--- src/Controller/ContentElement/TableOfContentsController.php
+++ src/Controller/ContentElement/TableOfContentsController.php
@@ -15,6 +15,7 @@ namespace Respinar\ContaoTocBundle\Controller\ContentElement;
 use Contao\ContentModel;
 use Contao\CoreBundle\Controller\ContentElement\AbstractContentElementController;
 use Contao\CoreBundle\DependencyInjection\Attribute\AsContentElement;
+use Contao\System;
 use Contao\Template;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -25,6 +26,22 @@ class TableOfContentsController extends AbstractContentElementController
     protected function getResponse(Template $template, ContentModel $model, Request $request): Response
     {
 
+        $template->tocTitle = $model->toc_title;
+        $template->className = $model->toc_className;
+        $template->headingSelector = $model->toc_headingSelector;
+
+        $request = System::getContainer()->get('request_stack')->getCurrentRequest();
+
+		if ($request && System::getContainer()->get('contao.routing.scope_matcher')->isBackendRequest($request))
+		{
+
+			$template->setName('be_table_of_contents');
+			$template->title = $model->toc_title;
+			$template->className = $model->toc_className;
+			$template->headings = $model->toc_headingSelector;
+
+			return $template->getResponse();
+		}
 
         $GLOBALS['TL_HEAD'][] = Template::generateScriptTag('bundles/respinarcontaotoc/js/toc.js', false, null);
         $GLOBALS['TL_HEAD'][] = Template::generateStyleTag('bundles/respinarcontaotoc/css/toc.css', false, null);
