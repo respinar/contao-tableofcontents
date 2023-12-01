60464807e2a4ef385bcd48fae8e4c1d945b229cd
Hamid Peywasti <hamid@respinar.com>
2023-12-01 18:18:51 +0330

    remove tocTitle field

--- contao/dca/tl_content.php
+++ contao/dca/tl_content.php
@@ -4,19 +4,12 @@ declare(strict_types=1);
 
 $GLOBALS['TL_DCA']['tl_content']['palettes']['table_of_contents'] = '
     {type_legend},type,headline;
-    {toc_legend},toc_title,toc_articleSelector,toc_headingSelector;
+    {toc_legend},toc_articleSelector,toc_headingSelector;
     {template_legend:hide},customTpl;
     {protected_legend:hide},protected;
     {expert_legend:hide},guests,cssID;
     {invisible_legend:hide},invisible,start,stop
 ';
-
-$GLOBALS['TL_DCA']['tl_content']['fields']['toc_title'] = array(
-	'exclude'       => true,
-    'inputType'     => 'text',
-    'eval'          => array('mandatory'=>true, 'maxlength'=>255, 'tl_class'=>'w50'),
-    'sql'           => "varchar(255) NOT NULL default ''"
-);
 
 $GLOBALS['TL_DCA']['tl_content']['fields']['toc_articleSelector'] = array(
     'exclude'                 => true,
