a1c19ad9d2c9db7a7b64b667855155d537c97873
Hamid Peywasti <hamid@respinar.com>
2024-11-30 11:15:45 +0330

    remove headline add toc_headline

--- contao/dca/tl_module.php
+++ contao/dca/tl_module.php
@@ -3,8 +3,8 @@
 declare(strict_types=1);
 
 $GLOBALS['TL_DCA']['tl_module']['palettes']['table_of_contents'] = '
-    {type_legend},name,headline,type;
-    {toc_legend},toc_articleSelector,toc_headingSelector;
+    {type_legend},name,type;
+    {toc_legend},toc_headline,toc_articleSelector,toc_headingSelector;
     {template_legend:hide},customTpl;
     {protected_legend:hide},protected;
     {expert_legend:hide},guests,cssID
